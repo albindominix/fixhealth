3e9267a3ce97bcb6bf67f89626099266b6a38c27
Albin Antony <albinantonydominic@gmail.com>
2024-01-25 18:30:18 +0530

    minor update

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -108,7 +108,6 @@ export default function ModalComponent() {
                         onClick={() => {
                           setShowModal(false);
                           reset();
-                          // setFile(null);
                         }}
                       >
                         Close
@@ -132,7 +131,6 @@ export default function ModalComponent() {
                         className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                         type="submit"
                         disabled={isSubmitting}
-                        //   onClick={() => setShowModal(false)}
                       >
                         Save Changes
                       </button>
